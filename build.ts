9a02a97cd4c5c8238043ab3bed1f25ef1a3da740
hadukinei <quiltqueen73344614@yahoo.co.jp>
2024-04-16 10:20:12 +0900

    Update build.ts

--- build.ts
+++ build.ts
@@ -1,29 +1,3 @@
-/*
-const { build } = require('esbuild')
-
-const entryFile = './src/index.ts'
-const shared = {
-  bundle: true,
-  entryPoints: [entryFile],
-  logLevel: 'info',
-  minify: true,
-  sourcemap: true,
-}
-
-build({
-  ...shared,
-  format: 'esm',
-  outfile: './dist/index.esm.js',
-  target: ['ES6'],
-})
-
-build({
-  ...shared,
-  format: 'cjs',
-  outfile: './dist/index.cjs.js',
-  target: ['ES6'],
-})
-*/
 import {build} from 'esbuild';
 
 const entryFile = ['./src/index.ts'];
