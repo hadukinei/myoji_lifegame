689de0d7f4379cebe576163f58b1c9b93a19b2c0
hadukinei <quiltqueen73344614@yahoo.co.jp>
2024-04-18 08:11:43 +0900

    ts

--- build-js.ts
+++ build-js.ts
@@ -1,26 +1,24 @@
-import {build} from 'esbuild';
+import {LogLevel, build} from 'esbuild';
 
 const entryFile = ['./src/index.ts'];
 
 const shared = {
-  bundle: true,
   entryPoints: entryFile,
+  target: ['ES6'],
+  bundle: true,
   minify: true,
   sourcemap: true,
+  logLevel: <LogLevel>'info',
 }
 
 build({
   ...shared,
   format: 'esm',
   outfile: './dist/index.esm.js',
-  target: ['ES6'],
-  logLevel: 'info',
 })
 
 build({
   ...shared,
   format: 'cjs',
   outfile: './dist/index.cjs.js',
-  target: ['ES6'],
-  logLevel: 'info',
 })
